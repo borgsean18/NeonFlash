50e44e14c75f5f77401a96ccb032941cf225e5c4
seanborg <seanborg8@gmail.com>
2023-08-28 20:01:12 +0100

    settings

--- Assets/Scripts/Camera/CustomCamera.cs
+++ Assets/Scripts/Camera/CustomCamera.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomCamera : MonoBehaviour
+namespace Camera
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CustomCamera : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Exposed Variables
+        [Header("Target")]
+        [SerializeField] private Transform targetTransform;
     }
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+namespace Managers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerManager : MonoBehaviour
     {
-        
-    }
+        // Exposed Variables
+        [Header("General")]
+        [SerializeField] private GameObject playerPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Private Variables
     }
 }
