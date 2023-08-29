3a4f9b5cd42a5dfa696aca0adca8b25bb6004aaf
seanborg <seanborg8@gmail.com>
2023-08-29 17:17:37 +0100

    Camera System

--- Assets/Scripts/Camera/CustomCamera.cs
+++ Assets/Scripts/Camera/CustomCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Camera
@@ -7,5 +8,46 @@ namespace Camera
         // Exposed Variables
         [Header("Target")]
         [SerializeField] private Transform targetTransform;
+        [SerializeField] private float maxDistToTarget;
+        [SerializeField] private float xOffsetToPlayer;
+
+        [Header("Clamps")]
+        [SerializeField] private float minYValue;
+
+
+        private void Update()
+        {
+            if (targetTransform == null) return;
+
+            FollowTarget();
+        }
+
+
+        public void SetCameraTarget(Transform _target)
+        {
+            targetTransform = _target;
+        }
+
+
+        private void FollowTarget()
+        {
+            Vector3 currentPos = transform.position;
+            Vector3 targetPos = targetTransform.position;
+            
+            targetPos.x += xOffsetToPlayer; // Keep camera slightly ahead of player
+            
+            float distToTarget = Vector2.Distance(currentPos, targetPos);
+
+            // Only proceed through here if the player is far enough from the camera
+            if (distToTarget < maxDistToTarget) 
+                return;
+
+            float newX = targetPos.x;
+            float newY = Mathf.Clamp(targetPos.y, minYValue, targetPos.y);
+
+            Vector3 newPos = new Vector3(newX, newY, currentPos.z);
+
+            transform.position = Vector3.Lerp(currentPos, newPos, Time.deltaTime * distToTarget);
+        }
     }
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -1,3 +1,6 @@
+using System;
+using Characters;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Managers
@@ -7,7 +10,34 @@ namespace Managers
         // Exposed Variables
         [Header("General")]
         [SerializeField] private GameObject playerPrefab;
+        
 
         // Private Variables
+        private bool _init;
+        private Player _player;
+
+
+        private void Awake()
+        {
+            if (!_init)
+                Init();
+        }
+
+
+        private void Init()
+        {
+            _init = true;
+            
+            SpawnPlayer();
+        }
+
+
+        private void SpawnPlayer()
+        {
+            _player = Instantiate(playerPrefab, new Vector3(0, 1, 0), quaternion.identity)
+                .GetComponent<Player>();
+
+            _player.Init();
+        }
     }
 }
