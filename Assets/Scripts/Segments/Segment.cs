255ffcf66d2008a67134f0b42385800510e9c884
seanborg <seanborg8@gmail.com>
2023-09-05 22:41:02 +0100

    add comments

--- Assets/Scripts/Segments/Segment.cs
+++ Assets/Scripts/Segments/Segment.cs
@@ -46,24 +46,32 @@ namespace Segments
 		{
 			float newX;
 			
+			// If this is the first segment in the biome
 			if (_prevSegment == null)
 			{
 				newX = _parentBiome.transform.position.x;
 
 				newX += SegmentWidth() / 2;
 				
+				// Spawn the segment at the Biomes XPos, adjusted for the segments width
 				transform.position = new Vector3(newX, 0);
 				
 				return;
 			}
 
+			// Get the XPos of the previous segment
 			float prevX = _prevSegment.transform.position.x;
+			
+			// Get the Width of the previous segment
 			float prevWidth = _prevSegment.SegmentWidth();
 
+			// New XPos is the previous segment XPos + (its width / 2)
 			newX = prevX + (prevWidth / 2);
 
+			// Add the Width / 2 of this segment to that XPos value
 			newX += SegmentWidth() / 2;
 
+			// Move this Segment to the newly calculated position (touching the previous segment)
 			transform.position = new Vector3(newX, 0);
 		}
 
