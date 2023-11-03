f7d8c653c1481477dd62e9d755f28c76720e6ff2
Sean Borg <seanborg@changingday.com>
2023-11-03 13:47:34 +0000

    WIP refactoring game manager

--- Assets/Scripts/MainManagers/GameManagerScript.cs
+++ Assets/Scripts/MainManagers/GameManagerScript.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace MainManagers
 {
@@ -13,6 +15,36 @@ namespace MainManagers
     
     public class GameManagerScript : MonoBehaviour
     {
+        // Exposed Variables
+        [Header("Events")]
+        [SerializeField] private UnityEvent startGame;
+        [SerializeField] private UnityEvent pauseGame;
+        [SerializeField] private UnityEvent loseGame;
         
+        
+        // Private Variables
+        private GameStates gameState;
+        private WorldManager worldManager;
+        
+        
+        // Properties
+        public GameStates GameState => gameState;
+        public UnityEvent StartGame => startGame;
+        public UnityEvent PauseGame => pauseGame;
+        public UnityEvent LoseGame => loseGame;
+
+
+        void Awake()
+        {
+            Init();
+        }
+
+
+        private void Init()
+        {
+            worldManager = GetComponent<WorldManager>();
+
+            gameState = GameStates.Idle;
+        }
     }
 }
