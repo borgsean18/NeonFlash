959fc2abfa16997eeea7cb385504bd5d65d33aed
seanborg <seanborg8@gmail.com>
2023-12-10 12:21:34 +0000

    Drone movement done

--- Assets/Scripts/Enemies/Drone/DroneBehaviour.cs
+++ Assets/Scripts/Enemies/Drone/DroneBehaviour.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
@@ -10,19 +11,31 @@ namespace Enemies.Drone
     {
         // Exposed Variables
         [Header("Movement Settings")]
+        [SerializeField] private float droneSpeed;
         [SerializeField] private float minTimeTillNextMovement;
         [SerializeField] private float maxTimeTillNextMovement;
         [SerializeField] private List<Vector3> droneAvailablePositions;
-        
+
 
         // Components
         private Animator _animator;
-        private Vector3 _targetPos;
-        
-        
+
+
         // Private Variables
+        private Vector3 _currentPos;
+        private Vector3 _targetPos;
         private bool _travel;
-        
+        private DroneStates _droneStates;
+
+
+        private enum DroneStates
+        {
+            None,
+            Idle,
+            Firing,
+            Moving
+        }
+
 
         private void Awake()
         {
@@ -33,6 +46,9 @@ namespace Enemies.Drone
         private void Init()
         {
             _animator = GetComponentInChildren<Animator>();
+            _targetPos = RandomPosition();
+
+            _droneStates = DroneStates.Moving;
         }
 
 
@@ -41,10 +57,44 @@ namespace Enemies.Drone
             MoveToPosition();
         }
 
-
+        
         private void MoveToPosition()
         {
+            // If the drone more or less arrived at its destination
+            if (Vector2.Distance(transform.position, _targetPos) < 0.2f)
+            {
+                // If drone is in moving state
+                if (_droneStates == DroneStates.Moving)
+                {
+                    // Set drone to idle
+                    _droneStates = DroneStates.Idle;
+
+                    // Pick a new target position after a timer expires
+                    StartCoroutine(PickNewPosition());
+                }
+                
+                // Do nothing more (Still if drone is at its target position)
+                return;
+            }
+            
+            // Move to target position
+            transform.position = Vector2.MoveTowards(
+                transform.position, 
+                _targetPos, 
+                Time.deltaTime * droneSpeed
+                );
+        }
+
+
+        private IEnumerator PickNewPosition()
+        {
+            float timeToWait = Random.Range(minTimeTillNextMovement, maxTimeTillNextMovement);
+            
+            yield return new WaitForSeconds(timeToWait);
+
             _targetPos = RandomPosition();
+
+            _droneStates = DroneStates.Moving;
         }
 
 
