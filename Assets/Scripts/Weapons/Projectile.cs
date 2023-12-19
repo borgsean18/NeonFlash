0f605f4245eb9e823646b69c7ae4d7f4dfe28ad5
Sean <seanborg8@gmail.com>
2023-12-19 16:33:43 +0000

    Create Projectile.cs

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+namespace Weapons
+{
+    public class DirectProjectile : MonoBehaviour
+    {
+        // Private Variables
+        private Transform _target;
+        private Vector3 _targetPos;
+        private GameObject _firer;
+    }
+}
