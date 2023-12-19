2889be010d5874399c31ea0627be8ef5c64edfb7
Sean <seanborg8@gmail.com>
2023-12-19 15:58:01 +0000

    Create RedirectProjectile.cs

---
+++ Assets/Scripts/Weapons/RedirectProjectile.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Weapons
+{
+    public class RedirectProjectile : MonoBehaviour
+    {
+        void Update()
+        {
+            
+        }
+    }
+}
