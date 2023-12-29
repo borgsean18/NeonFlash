8131e12bbd17798cf77582c51fa1b1d906a26193
Sean Borg <seanborg@changingday.com>
2023-12-29 16:36:31 +0000

    drone dies even better

--- Assets/Scripts/Enemies/Drone/DroneTakeDamage.cs
+++ Assets/Scripts/Enemies/Drone/DroneTakeDamage.cs
@@ -1,3 +1,4 @@
+using System;
 using Combat;
 using UnityEngine;
 
@@ -10,9 +11,28 @@ namespace Enemies.Drone
         [SerializeField] private DroneBehaviour droneBehaviour;
         
         
+        // Components
+        private Animator anim;
+        private Rigidbody2D rb;
+
+
+        void Awake()
+        {
+            anim = GetComponent<Animator>();
+
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+
         protected override void Die()
         {
             droneBehaviour.Die();
+
+            // Fall to the ground
+            rb.gravityScale = 1;
+            
+            // Stop all animations
+            anim.enabled = false;
         }
     }
 }
